74b34a036cbba4d623dc30c294e701aa77a09c52
Darlan Sá <darlansaa@gmail.com>
2022-05-25 16:09:44 -0300

    [parameters, execption]  passando itens e subindo exceções

--- sessao3/main.py
+++ sessao3/main.py
@@ -1,21 +1,30 @@
-from fastapi import FastAPI
+from distutils.log import debug
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
 
 cursos = {
-    1:{"Titulo": "Programação para Leigos",
-       "aulas": 112,
-       "horas": 58, },
-    2:{"Titulo": "Algorimo e Lógica de Programação",
-       "aulas": 100,
-       "horas": 60, },
-    3:{"Titulo": "FastAPI - APIs Modernas e Assíncronas com Python",
-       "aulas": 150,
-       "horas": 80, },
-    4:{"Titulo": "Banco de dados para Leigos",
-       "aulas": 120,
-       "horas": 90, },
+    1: {
+        "Titulo": "Programação para Leigos",
+        "aulas": 112,
+        "horas": 58,
+    },
+    2: {
+        "Titulo": "Algorimo e Lógica de Programação",
+        "aulas": 100,
+        "horas": 60,
+    },
+    3: {
+        "Titulo": "FastAPI - APIs Modernas e Assíncronas com Python",
+        "aulas": 150,
+        "horas": 80,
+    },
+    4: {
+        "Titulo": "Banco de dados para Leigos",
+        "aulas": 120,
+        "horas": 90,
+    },
 }
 
 
@@ -24,9 +33,22 @@ async def get_cursos():
     return cursos
 
 
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não Encontrado")
+        
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app", host="0.0.0.0", port=8000, log_level="info", reload=True
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_level="info",
+        reload=True,
+        debug=True,
     )
