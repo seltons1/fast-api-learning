7624fcb4c5e7b51c365dad7f30e91df4994451d2
Darlan Sá <darlansaa@gmail.com>
2022-05-26 17:21:07 -0300

    [refact + docs] refatorando o fake db, melhorando a documentação.

--- sessao3/models.py
+++ sessao3/models.py
@@ -7,3 +7,18 @@ class Curso(BaseModel):
     titulo: str
     aulas: str
     horas: int
+
+
+cursos = [
+    Curso(id=1, titulo="Programação para Leigos", aulas=112, horas=58),
+    Curso(
+        id=2, titulo="Algorimo e Lógica de Programação", aulas=100, horas=90
+    ),
+    Curso(
+        id=3,
+        titulo="FastAPI - APIs Modernas e Assíncronas com Python",
+        aulas=150,
+        horas=100,
+    ),
+    Curso(id=4, titulo="Banco de dados para Leigos", aulas=120, horas=90),
+]
