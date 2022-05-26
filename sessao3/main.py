f36a4c3b17ef3577c76e3cbbe901a4f65bc15198
Darlan Sá <darlansaa@gmail.com>
2022-05-26 09:44:23 -0300

    [pathP] Implementando path parameters

--- sessao3/main.py
+++ sessao3/main.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from fastapi import FastAPI, HTTPException, status, Response
+from fastapi import FastAPI, HTTPException, status, Response, Path
 from fastapi.responses import JSONResponse
 from models import Curso
 
@@ -37,7 +37,7 @@ async def get_cursos():
 
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(default= None, title= 'ID do curso', description= 'Deve ser entre 1 e 4', gt= 0, lt= 5)):
     try:
         curso = cursos[curso_id]
         return curso
