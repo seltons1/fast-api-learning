0bf5b2224c5f6c28f1e4aa86c76c0efee030582a
Darlan Sá <darlansaa@gmail.com>
2022-05-27 18:11:32 -0300

    [testes] realizado testes, ajustado erros e formatação via black

--- sessao5/criar_tabelas.py
+++ sessao5/criar_tabelas.py
@@ -1,16 +1,19 @@
 from sqlmodel import SQLModel
 from core.database import engine
 
-async def create_tables()-> None:
+
+async def create_tables() -> None:
     import models.__all_models
+
     print("criando as tabelas")
-    
+
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
     print("tabelas criadas com sucesso")
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     import asyncio
-    
+
     asyncio.run(create_tables())
