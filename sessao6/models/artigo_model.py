26879abc460da89b9894adfe139e4b70acf9b527
Darlan Sá <darlansaa@gmail.com>
2022-05-28 17:39:05 -0300

    [criar tabelas + ajustes] criação das tabelas e correção de bugs

--- sessao6/models/artigo_model.py
+++ sessao6/models/artigo_model.py
@@ -5,7 +5,7 @@ from core.configs import settings
 
 class ArtigoModel(settings.DBBaseModel):
     __tablename__ = "artigos"
-    id = Column(Integer, primary_key=True, autoincremet=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     titulo = Column(String(256))
     url_fonte = Column(String(256))
     descricao = Column(String(256))
